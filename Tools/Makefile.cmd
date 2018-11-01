cbdce95134e1217bbbfaa4870519700b30de4a57
ray <rayhuang@canyon-semi.com.tw>
2018-11-01 14:21:13 +0800

    restart

--- Tools/Makefile.cmd
+++
@@ -1,7 +0,0 @@
-rd /s/q Release
-md .\Release
-copy .\Objects\*.hex .\Release\*.hex
-copy .\Objects\*.bin .\Release\*.bin
-copy .\Rev.txt .\Release\Rev.txt
-del /s .\Objects\*.bin
-start .\Tools\CheckFileSize.exe .\Release\CY2311_R2.bin 8192
