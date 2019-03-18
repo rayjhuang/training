c3426dc78596019fc2b92df12cc0b947c6b16f79
ray <rayhuang@canyon-semi.com.tw>
2019-03-18 12:01:38 +0800

    update duplicate_repository.sh

--- duplicate_repository.sh
+++ duplicate_repository.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ##
 ## for en exist repository, $source, and an exist empty one, $target,
 ## clone the $source into the ./temp subdirectory
@@ -8,21 +9,33 @@
 ## rayjhuang@msn.com 2019/03/15
 ## All Rights Are Reserved
 ##
-   source='https://github.com/rayjhuang/train1'
-   target='https://github.com/rayjhuang/train2'
+   source='ssh://git@10.12.3.237:/mnt/app1/git/FW//CAN1108/emarker'
+   target='https://github.com/rayjhuang/CY2517'
+
+   if [ -d ./temp ]; then
+      echo Remove ./temp
+      rm -rf ./temp
+   fi
 
    git clone $source temp
-   cd ./temp
-   git branch -a
-   for branch in `git branch -a | grep '^ *remotes\/origin\/' \
-	                        | grep -v 'master' \
-				| grep -v 'HEAD'`; do
-      local_branch=`echo $branch | cut -d '/' -f3`
-      git checkout $branch -b $local_branch -f
-   done
-   git checkout master
-   git push --all  --repo=$target
-   git push --tags --repo=$target
-   cd ..
-   rm -rf ./temp
+
+   if [ ! -d ./temp ]; then
+      echo No repository created
+   else
+      cd ./temp
+      git branch -a
+      for branch in `git branch -a | grep '^ *remotes\/origin\/' \
+	                           | grep -v 'master' \
+		   		   | grep -v 'HEAD'`; do
+         local_branch=`echo $branch | cut -d '/' -f3`
+         git checkout $branch -b $local_branch -f
+      done
+      git checkout master
+
+      git push --all  --repo=$target -f
+      git push --tags --repo=$target -f
+
+      cd ..
+      rm -rf ./temp
+   fi
 
